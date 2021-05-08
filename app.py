580fbb5b8bdf6e6b27fce089ba5a8a49248d87bb
Anusha-Oorugonda <anusha.official96@gmail.com>
2021-05-08 15:51:43 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify,  request, render_template
+import joblib
+import numpy as np
+from model import SentimentAnalysis
+import pandas as pd
+import numpy as np
+from collections import Counter
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import pickle
+
+s=SentimentAnalysis()
+app = Flask(__name__)
+
+print("in app")
+
+@app.route('/')
+def home():
+    df_final=pd.read_csv("processed_txts.csv")
+    s.lr_model(df_final)
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if (request.method == 'POST'):
+        user_name=request.form["reviews_username"]
+        print(user_name)
+        df_final=pd.read_csv("processed_txts.csv")
+        if user_name not in df_final["reviews_username"].tolist():
+            return render_template('index.html', prediction_text='user name not exists')
+        #print(df_final.head(1))
+        top5_df=s.user_recomend(df_final,user_name)
+        print(top5_df)
+        top5_df=top5_df[["name","brand","manufacturer"]]
+        #output=top5_df
+        return render_template('index.html', columns=top5_df.columns.values, rows=list(top5_df.values.tolist()), zip=zip)
+        #return render_template('index.html', prediction_text='Sentiment Output {}'.format(output))
+    else :
+        return render_template('index.html')
+    
+#@app.route("/predict_api", methods=['POST', 'GET'])
+#def predict_api():
+    #print(" request.method :",request.method)
+    #if (request.method == 'POST'):
+        #data = request.get_json()
+        #return jsonify(model_load.predict([np.array(list(data.values()))]).tolist())
+    #else:
+        #return render_template('index.html')
+
+        
+if __name__ == '__main__':
+    app.run(debug=False)
---
+++ model.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+import warnings
+warnings.filterwarnings('ignore')
+import nltk
+nltk.download('wordnet')
+from nltk.stem import PorterStemmer
+from textblob import Word
+from sklearn.model_selection import train_test_split
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+import joblib
+from collections import Counter
+
+class SentimentAnalysis():
+    def __init__(self):
+        print("initialised")
+    #def load_preprocess(self):
+        #reviews_df=pd.read_csv('sample30.csv',encoding='latin-1')
+        #reviews_df['reviews_text'] = reviews_df['reviews_text'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+        #reviews_df['reviews_text'] = reviews_df['reviews_text'].str.replace('[^\w\s]','')
+        #reviews_df['reviews_text'] = reviews_df['reviews_text'].apply(lambda x: " ".join(x for x in x.split() if x not in stop_words))
+        #freq_ten = pd.Series(' '.join(reviews_df['reviews_text']).split()).value_counts()[:10]
+        #freq_ten = list(freq_ten.index)
+        #reviews_df['reviews_text'] = reviews_df['reviews_text'].apply(lambda x: " ".join(x for x in x.split() if x not in freq_ten))
+        #freq_rare = pd.Series(' '.join(reviews_df['reviews_text']).split()).value_counts()[-10:]
+        #freq_rare = list(freq_rare.index)
+        #reviews_df['reviews_text'] = reviews_df['reviews_text'].apply(lambda x: " ".join(x for x in x.split() if x not in freq_rare))
+        #st = PorterStemmer()
+        #reviews_df['reviews_text'].apply(lambda x: " ".join([st.stem(word) for word in x.split()]))
+        #reviews_df['reviews_text'] = reviews_df['reviews_text'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+        #reviews_df['user_sentiment']=reviews_df['user_sentiment'].map({"Positive":1,"Negative":0})
+        #reviews_df = reviews_df.dropna(subset=['user_sentiment'])
+        #print(reviews_df.head(1))
+        #return reviews_df
+    def lr_model(self,df):
+        X = df['reviews_text']
+        y = df['user_sentiment']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=43)
+        #print('X_train', X_train.shape)
+        #print('y_train', y_train.shape)
+        #print('X_test', X_test.shape)
+        #print('y_test', y_test.shape)
+        tfidf_vect = TfidfVectorizer()
+        train_tfidf = tfidf_vect.fit_transform(X_train.values.astype('U'))
+        test_tfidf=tfidf_vect.transform(X_test.values.astype('U'))
+        LRModel=LogisticRegression(C=10,class_weight={0:0.6,1:0.4},penalty='l2',random_state=43)
+        LRModel.fit(train_tfidf, y_train)
+        joblib.dump(LRModel, 'LRModel.pkl')
+        joblib.dump(tfidf_vect,"tfidf_vect.pkl")
+        #return tfidf_vect   
+    def user_recomend(self,df,user_name):
+        model_load = joblib.load("LRModel.pkl")
+        tfidf_vect=joblib.load("tfidf_vect.pkl")
+        #reviews_df=load_preprocess(df)
+        reviews_df=df.groupby(['reviews_username','name'])['reviews_rating'].mean().reset_index()
+        train, test = train_test_split(reviews_df, test_size=0.30, random_state=43)
+        # Pivot the train ratings' dataset into matrix format in which columns are movies and the rows are user IDs.
+        user_pivot = train.pivot(index='reviews_username',columns='name',values='reviews_rating').fillna(0)
+        dummy_train = train.copy()
+        dummy_train['reviews_rating'] = dummy_train['reviews_rating'].apply(lambda x: 0 if x>=1 else 1)
+        dummy_train = dummy_train.pivot(index='reviews_username',columns='name',values='reviews_rating').fillna(1)
+        # Create a user-movie matrix.
+        user_pivot = train.pivot(index='reviews_username',columns='name',values='reviews_rating')
+        mean = np.nanmean(user_pivot, axis=1)
+        df_subtracted = (user_pivot.T-mean).T
+        user_correlation = 1 - pairwise_distances(df_subtracted.fillna(0), metric='cosine')
+        user_correlation[np.isnan(user_correlation)] = 0
+        user_correlation[user_correlation<0]=0
+        user_predicted_ratings = np.dot(user_correlation, user_pivot.fillna(0))
+        user_final_rating = np.multiply(user_predicted_ratings,dummy_train)
+        final_recomended_prods=user_final_rating.loc[user_name].sort_values(ascending=False)[0:20]
+        top20_prod={}
+        top20_prod['name']=[]
+        top20_prod["brand"]=[]
+        top20_prod["categories"]=[]
+        top20_prod['manufacturer']=[]
+        recom_prod=list(final_recomended_prods.index)
+        pos_rated_prod=[]
+        for i in range(20):
+            top20_prod["brand"].append(df.loc[df.name==recom_prod[i],"brand"].values[0])
+            top20_prod["categories"].append(df.loc[df.name==recom_prod[i],"categories"].values[0])
+            top20_prod['manufacturer'].append(df.loc[df.name==recom_prod[i],"manufacturer"].values[0])
+            top20_prod['name'].append(final_recomended_prods.index[i])
+            reviews_texts=np.array(df.loc[df.name==recom_prod[i],"reviews_text"]).astype('U')
+            y=Counter(model_load.predict(tfidf_vect.transform(reviews_texts)))[1]
+            pos_rated_prod.append(y)                          
+        top20_df=pd.DataFrame(top20_prod)
+        #top20_df
+        top20_df["positive_ratings"]=pos_rated_prod
+        top5_df=top20_df.sort_values(by="positive_ratings",ascending=False)[:5]
+        return top5_df
+   
+        
+    
